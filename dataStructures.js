0b312a64f6df17e5e3f670a4b03a3808e3adad2d
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2022-08-09 11:48:09 -0500

    binary search tree

--- dataStructures.js
+++ dataStructures.js
@@ -280,3 +280,60 @@ class Queue{
         return temp.value;
     }
 }
+
+// BINARY SEARCH TREE
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+    insert(value){
+        let newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        } else {
+            let current = this.root;
+            while(true){
+                if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if(value > current.value){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+    }
+    find(value){
+        if(this.root === null) return false;
+        let current = this.root, found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current;
+    }
+}
